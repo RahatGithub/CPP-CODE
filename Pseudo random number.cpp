8b2191a6de70aa84d231766b8a4d387c44ecc4d8
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2021-11-04 12:25:09 +0600

    Pseudo and Truly random number added

---
+++ Pseudo random number.cpp
@@ -0,0 +1,19 @@
+/*Generating pseudo random numbers:
+
+This program generates random numbers but every time 
+the program is run, it generates the same random numbers.
+Hence these are called 'Pseudo random numbers'.*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+int main() {
+
+    for(int i=0; i<10; i++)
+    {
+        cout << (rand() % 10) << endl; //rand() generates any random number, whereas (rand() % n) generates within the range 0-n
+    }
+    
+}
